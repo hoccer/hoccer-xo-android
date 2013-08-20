37a2f6d795063c079e0976aadb6547639c8a8f7e
Ingo Albrecht <prom@berlin.ccc.de>
2013-08-21 01:29:58 +0200

    android: nicer and i18nable status view with autohide

--- src/main/java/com/hoccer/talk/android/fragment/StatusFragment.java
+++ src/main/java/com/hoccer/talk/android/fragment/StatusFragment.java
@@ -1,5 +1,6 @@
 package com.hoccer.talk.android.fragment;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
@@ -67,15 +68,45 @@ public class StatusFragment extends TalkFragment {
     }
 
     private void applyClientState(int state) {
-        final String stateString = HoccerTalkClient.stateToString(state);
-
-        mStatusText.setText(stateString);
+        String statusText;
+        Resources r = getResources();
+        switch(state) {
+            case HoccerTalkClient.STATE_IDLE:
+                statusText = r.getString(R.string.client_state_idle);
+                break;
+            case HoccerTalkClient.STATE_INACTIVE:
+                statusText = r.getString(R.string.client_state_inactive);
+                break;
+            case HoccerTalkClient.STATE_ACTIVE:
+                statusText = r.getString(R.string.client_state_active);
+                break;
+            case HoccerTalkClient.STATE_CONNECTING:
+                statusText = r.getString(R.string.client_state_connecting);
+                break;
+            case HoccerTalkClient.STATE_LOGIN:
+                statusText = r.getString(R.string.client_state_login);
+                break;
+            case HoccerTalkClient.STATE_RECONNECTING:
+                statusText = r.getString(R.string.client_state_reconnecting);
+                break;
+            case HoccerTalkClient.STATE_REGISTERING:
+                statusText = r.getString(R.string.client_state_registering);
+                break;
+            case HoccerTalkClient.STATE_SYNCING:
+                statusText = r.getString(R.string.client_state_syncing);
+                break;
+            default:
+                statusText = r.getString(R.string.client_state_unknown);
+                break;
+        }
+        mStatusText.setText(statusText);
 
         final FragmentTransaction tr =
                 getActivity()
                         .getSupportFragmentManager()
                         .beginTransaction();
-        if (stateString.equals("active")) {
+        if (state == HoccerTalkClient.STATE_ACTIVE
+                || state == HoccerTalkClient.STATE_IDLE) {
             tr.hide(StatusFragment.this);
         } else {
             tr.show(StatusFragment.this);
