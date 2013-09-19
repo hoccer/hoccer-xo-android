d93def886d5d87441e0ce7505163da245a8950a5
Ingo Albrecht <prom@berlin.ccc.de>
2013-09-19 18:00:05 +0200

    android: prettier token dialog

--- src/main/java/com/hoccer/talk/android/dialog/TokenDialog.java
+++ src/main/java/com/hoccer/talk/android/dialog/TokenDialog.java
@@ -2,12 +2,17 @@ package com.hoccer.talk.android.dialog;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.ContentResolver;
 import android.content.DialogInterface;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.RemoteException;
+import android.provider.ContactsContract;
 import com.actionbarsherlock.app.SherlockDialogFragment;
 import com.hoccer.talk.android.TalkActivity;
 import com.hoccer.talk.client.model.TalkClientSmsToken;
+import com.hoccer.xo.R;
 import org.apache.log4j.Logger;
 
 public class TokenDialog extends SherlockDialogFragment implements DialogInterface.OnClickListener {
@@ -27,13 +32,36 @@ public class TokenDialog extends SherlockDialogFragment implements DialogInterfa
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
+        // XXX similar code in RichtContactsAdapter.updateToken()
+        ContentResolver resolver = mActivity.getContentResolver();
+        Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(mToken.getSender()));
+
+        String name = mToken.getSender();
+        String photo = "content://" + R.drawable.avatar_default_contact;
+
+        Cursor cursor = resolver.query(uri,
+                new String[] {
+                        ContactsContract.PhoneLookup.DISPLAY_NAME,
+                },
+                null, null, null);
+
+        if(cursor != null && cursor.getCount() > 0) {
+            int nameIndex = cursor.getColumnIndex(ContactsContract.Data.DISPLAY_NAME);
+            cursor.moveToFirst();
+            name = cursor.getString(nameIndex);
+        }
+
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getSherlockActivity());
 
+        // XXX i18n mess
         builder.setTitle("Invitation");
         builder.setCancelable(true);
         builder.setNegativeButton("Decline", this);
         builder.setPositiveButton("Accept", this);
         builder.setNeutralButton("Cancel", this);
+
+        builder.setMessage(name + " has sent you an invitation via SMS.\nDo you wish to accept?");
 
         return builder.create();
     }
