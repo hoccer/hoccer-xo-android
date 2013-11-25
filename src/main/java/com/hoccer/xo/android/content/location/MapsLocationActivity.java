d457c9cd5782efd4c465a237df89772b00df19e2
Ingo Albrecht <prom@berlin.ccc.de>
2013-11-25 07:11:51 +0100

    android: show own location on map

--- src/main/java/com/hoccer/xo/android/content/location/MapsLocationActivity.java
+++ src/main/java/com/hoccer/xo/android/content/location/MapsLocationActivity.java
@@ -1,9 +1,15 @@
 package com.hoccer.xo.android.content.location;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.hoccer.xo.android.base.XoActivity;
 import com.hoccer.xo.release.R;
 
 public class MapsLocationActivity extends XoActivity {
+
+    SupportMapFragment mMapFragment;
+
+    GoogleMap mMap;
 
     @Override
     protected int getLayoutResource() {
@@ -13,6 +19,24 @@ public class MapsLocationActivity extends XoActivity {
     @Override
     protected int getMenuResource() {
         return -1;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // get the map fragment
+        if(mMapFragment == null) {
+            mMapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        }
+        // get and configure the map behind the fragment
+        if(mMap == null) {
+            mMap = mMapFragment.getMap();
+            setupMap();
+        }
+    }
+
+    private void setupMap() {
+        mMap.setMyLocationEnabled(true);
     }
 
 }
