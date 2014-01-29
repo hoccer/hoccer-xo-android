85b61dd73b16aabd1abe7d0558965876bebd4eb0
Julian Krumow <julian.krumow@artcom.de>
2014-01-29 18:09:40 +0100

    ContentLength is correctly set now. Added retrieval of file size of vcard.

--- src/main/java/com/hoccer/xo/android/content/vcard/ContactSelector.java
+++ src/main/java/com/hoccer/xo/android/content/vcard/ContactSelector.java
@@ -2,12 +2,15 @@ package com.hoccer.xo.android.content.vcard;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.ContactsContract;
 import com.hoccer.xo.android.content.SelectedContent;
 import com.hoccer.xo.android.content.IContentSelector;
 import org.apache.log4j.Logger;
+
+import java.io.FileNotFoundException;
 
 public class ContactSelector implements IContentSelector {
 
@@ -38,18 +41,36 @@ public class ContactSelector implements IContentSelector {
         int lookupKeyIndex = cursor.getColumnIndex(columns[0]);
         String lookupKey = cursor.getString(lookupKeyIndex);
 
-        String contentUri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_VCARD_URI, lookupKey).toString();
+        Uri contentUri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_VCARD_URI, lookupKey);
+        String contentUriPath = contentUri.toString();
 
         // XXX what the heck!? the android constant seems broken.
-        if(contentUri.startsWith("content:/com.android.contacts")) {
-            contentUri = contentUri.replace("content:/com.android.contacts", "content://com.android.contacts");
+        if(contentUriPath.startsWith("content:/com.android.contacts")) {
+            contentUriPath = contentUriPath.replace("content:/com.android.contacts", "content://com.android.contacts");
         }
 
         cursor.close();
 
-        SelectedContent co = new SelectedContent(intent, contentUri);
+        SelectedContent co = new SelectedContent(intent, contentUriPath);
         co.setContentType(ContactsContract.Contacts.CONTENT_VCARD_TYPE);
         co.setContentMediaType("vcard");
+
+
+        AssetFileDescriptor fd = null;
+        long fileSize = 0;
+
+        try {
+            fd = context.getContentResolver().openAssetFileDescriptor(contentUri, "r");
+
+            //FileInputStream fis = fd.createInputStream();
+            //byte[] buf = new byte[(int) fd.getDeclaredLength()];
+            //fis.read(buf);
+            fileSize = fd.getLength();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        co.setContentLength((int)fileSize);
 
         return co;
     }
