bbc8e7f5420fe9b3c16e55c578473a8651e7fc81
Julian Krumow <julian.krumow@artcom.de>
2014-03-10 13:11:49 +0100

    Extracted dialogs from GroupProfileFragment.

--- src/main/java/com/hoccer/xo/android/XoDialogs.java
+++ src/main/java/com/hoccer/xo/android/XoDialogs.java
@@ -3,9 +3,7 @@ package com.hoccer.xo.android;
 import com.hoccer.talk.client.model.TalkClientContact;
 import com.hoccer.talk.client.model.TalkClientSmsToken;
 import com.hoccer.xo.android.base.XoActivity;
-import com.hoccer.xo.android.dialog.ContactDeleteDialog;
-import com.hoccer.xo.android.dialog.GroupManageDialog;
-import com.hoccer.xo.android.dialog.TokenDialog;
+import com.hoccer.xo.android.dialog.*;
 
 /**
  * Static dialog launchers
@@ -18,6 +16,9 @@ public class XoDialogs {
     public final static String DIALOG_TOKEN = "TokenDialog";
     public final static String DIALOG_CONTACT_DELETE = "ContactDeleteDialog";
     public final static String DIALOG_CONTACT_DEPAIR = "ContactDepairDialog";
+    public final static String DIALOG_GROUP_DELETE = "GroupDeleteDialog";
+    public final static String DIALOG_GROUP_REJECT_INVITATION = "GroupRejectInvitationDialog";
+    public final static String DIALOG_GROUP_LEAVE = "GroupLeaveDialog";
     public final static String DIALOG_GROUP_KICK = "GroupKickDialog";
     public final static String DIALOG_GROUP_INVITE = "GroupManageDialog";
     public final static String DIALOG_GROUP_MANAGE = "GroupManageDialog";
@@ -25,6 +26,21 @@ public class XoDialogs {
     public static void confirmDeleteContact(XoActivity activity, TalkClientContact contact) {
         new ContactDeleteDialog(activity, contact)
                 .show(activity.getFragmentManager(), DIALOG_CONTACT_DELETE);
+    }
+
+    public static void confirmDeleteGroup(XoActivity activity, TalkClientContact group) {
+        new GroupDeleteDialog(activity, group)
+                .show(activity.getFragmentManager(), DIALOG_GROUP_DELETE);
+    }
+
+    public static void confirmRejectInvitationGroup(XoActivity activity, TalkClientContact group) {
+        new GroupRejectInvitationDialog(activity, group)
+                .show(activity.getFragmentManager(), DIALOG_GROUP_REJECT_INVITATION);
+    }
+
+    public static void confirmLeaveGroup(XoActivity activity, TalkClientContact group) {
+        new GroupLeaveDialog(activity, group)
+                .show(activity.getFragmentManager(), DIALOG_GROUP_LEAVE);
     }
 
     public static void selectGroupManage(XoActivity activity, TalkClientContact group) {
