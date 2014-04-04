27179c0abd95b20179bddfe5bed3cb89bba671ca
jacob <jacob.zschunke@artcom.de>
2014-04-04 12:11:01 +0200

    added export feature to the android client. import still missing

--- src/main/java/com/hoccer/xo/android/activity/XoPreferenceActivity.java
+++ src/main/java/com/hoccer/xo/android/activity/XoPreferenceActivity.java
@@ -1,46 +1,60 @@
 package com.hoccer.xo.android.activity;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.DialogInterface;
-import android.content.SharedPreferences;
-import android.graphics.drawable.ColorDrawable;
-import android.os.Handler;
-import android.os.Message;
-import android.preference.PreferenceManager;
-import android.preference.PreferenceScreen;
-import android.view.*;
 import com.hoccer.xo.android.XoApplication;
 import com.hoccer.xo.android.XoConfiguration;
 import com.hoccer.xo.android.view.AttachmentTransferControlView;
 import com.hoccer.xo.release.R;
 
 import net.hockeyapp.android.CrashManager;
+
 import org.apache.log4j.Logger;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.sql.SQLException;
 
-public class XoPreferenceActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+public class XoPreferenceActivity extends PreferenceActivity
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     private static final Logger LOG = Logger.getLogger(XoPreferenceActivity.class);
+
     private AttachmentTransferControlView mSpinner;
+
     private Handler mDialogDismisser;
+
     private Dialog mWaitingDialog;
 
-            @Override
-            protected void onCreate(Bundle savedInstanceState) {
-                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-                preferences.registerOnSharedPreferenceChangeListener(this);
-                LOG.debug("onCreate()");
-                super.onCreate(savedInstanceState);
-                getActionBar().setDisplayHomeAsUpEnabled(true);
-                if (XoConfiguration.DEVELOPMENT_MODE_ENABLED) {
-                    addPreferencesFromResource(R.xml.development_preferences);
-                } else {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        preferences.registerOnSharedPreferenceChangeListener(this);
+        LOG.debug("onCreate()");
+        super.onCreate(savedInstanceState);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        if (XoConfiguration.DEVELOPMENT_MODE_ENABLED) {
+            addPreferencesFromResource(R.xml.development_preferences);
+        } else {
             addPreferencesFromResource(R.xml.preferences);
         }
     }
@@ -71,14 +85,15 @@ public class XoPreferenceActivity extends PreferenceActivity implements SharedPr
     }
 
     public void createDialog() {
-        LayoutInflater inflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
         View view = inflater.inflate(R.layout.waiting_dialog, null);
         mSpinner = (AttachmentTransferControlView) view.findViewById(R.id.content_progress);
 
         mWaitingDialog = new Dialog(this);
         mWaitingDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         mWaitingDialog.setContentView(view);
-        mWaitingDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        mWaitingDialog.getWindow()
+                .setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
         mWaitingDialog.setCanceledOnTouchOutside(false);
         if (!isFinishing()) {
             mWaitingDialog.show();
@@ -109,7 +124,7 @@ public class XoPreferenceActivity extends PreferenceActivity implements SharedPr
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        if(key.equals("preference_keysize")) {
+        if (key.equals("preference_keysize")) {
             createDialog();
             regenerateKeys();
         }
@@ -138,4 +153,65 @@ public class XoPreferenceActivity extends PreferenceActivity implements SharedPr
         }
         super.onDestroy();
     }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference.getKey().equals("preference_export")) {
+            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+            final EditText passwordInput = new EditText(this);
+            passwordInput.setInputType(EditorInfo.TYPE_TEXT_VARIATION_PASSWORD);
+            dialogBuilder.setTitle(R.string.export_credentials_dialog_title);
+            dialogBuilder
+                    .setPositiveButton(R.string.common_yes, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            String password = passwordInput.getText().toString();
+                            if (password != null && password.length() > 0) {
+                                exportCredentials(password);
+                                dialog.dismiss();
+                            } else {
+                                Toast.makeText(XoPreferenceActivity.this, R.string.no_password,
+                                        Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+            dialogBuilder
+                    .setNegativeButton(R.string.common_no, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            passwordInput.setText("");
+                            dialog.dismiss();
+                        }
+                    });
+
+            dialogBuilder.setView(passwordInput);
+
+        } else if (preference.getKey().equals("preference_import")) {
+
+        }
+
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    private void exportCredentials(String password) {
+        try {
+            byte[] credentialsContainer = XoApplication.getXoClient()
+                    .makeCryptedCredentialsContainer(XoApplication.getXoClient().getSelfContact(),
+                            password);
+
+            FileOutputStream fos = new FileOutputStream(
+                    XoApplication.getAttachmentDirectory() + "credentials/credentials.json");
+            fos.write(credentialsContainer);
+            fos.flush();
+            fos.close();
+        } catch (IOException e) {
+            LOG.error("error while writing credentials container to filesyystem.", e);
+            Toast.makeText(this, R.string.export_credentials_failure, Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            LOG.error("error while generating credentials container", e);
+            Toast.makeText(this, R.string.export_credentials_failure, Toast.LENGTH_LONG).show();
+        }
+        Toast.makeText(this, R.string.export_credentials_success, Toast.LENGTH_LONG).show();
+    }
+
 }
