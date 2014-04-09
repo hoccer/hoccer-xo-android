7dfb7e56c9a280288bc371775ab1cc8ad1425566
Julian Krumow <julian.krumow@artcom.de>
2014-04-09 17:28:09 +0200

    Added replacement for Vec2D and Vec3D classes.

---
+++ src/main/java/com/hoccer/xo/android/gesture/Vec2D.java
@@ -0,0 +1,13 @@
+package com.hoccer.xo.android.gesture;
+
+
+public class Vec2D {
+
+    public Vec2D(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float x;
+    public float y;
+}
