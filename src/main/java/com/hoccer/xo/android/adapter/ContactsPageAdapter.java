e5c2aadb74b48613ad186fb5cf2a6906fbef477f
Igor Zubov <igor.zubov@artcom.de>
2014-04-09 13:21:46 +0200

    added library. PLEASE, make maven reimport

---
+++ src/main/java/com/hoccer/xo/android/adapter/ContactsPageAdapter.java
@@ -0,0 +1,22 @@
+package com.hoccer.xo.android.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class ContactsPageAdapter extends FragmentPagerAdapter {
+
+    public ContactsPageAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
