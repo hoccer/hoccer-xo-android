031aa3a8693bfde1133d1683a003b9cf6ae824fe
Julian Krumow <julian.krumow@artcom.de>
2014-06-05 13:00:49 +0200

    TransferAgent: - changed listener collection from Set to List, changed loops so the list can be mutated while iterating over it
    ContentItems:
    - made cell height flexible
    
    ChatVideoItem:
    - implemented ChatVideoItem
    
    ChatContactItem:
    - removed old ContactView
    - added VCard parsing
    - setting values for view elements on contact layout

--- src/main/java/com/hoccer/xo/android/view/chat/attachments/ChatVideoItem.java
+++ src/main/java/com/hoccer/xo/android/view/chat/attachments/ChatVideoItem.java
@@ -1,11 +1,18 @@
 package com.hoccer.xo.android.view.chat.attachments;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 import com.hoccer.talk.client.model.TalkClientMessage;
 import com.hoccer.talk.content.IContentObject;
+import com.hoccer.xo.android.base.XoActivity;
 import com.hoccer.xo.android.view.chat.ChatMessageItem;
 import com.hoccer.xo.release.R;
 
@@ -28,7 +35,7 @@ public class ChatVideoItem extends ChatMessageItem {
     }
 
     @Override
-    protected void displayAttachment(IContentObject contentObject) {
+    protected void displayAttachment(final IContentObject contentObject) {
         super.displayAttachment(contentObject);
 
         // add view lazily
@@ -38,7 +45,42 @@ public class ChatVideoItem extends ChatMessageItem {
             mContentWrapper.addView(videoLayout);
         }
 
+        TextView videoTitle = (TextView) mContentWrapper.findViewById(R.id.tv_video_title);
+        TextView videoDescription = (TextView) mContentWrapper.findViewById(R.id.tv_video_description);
+        ImageButton playButton = (ImageButton) mContentWrapper.findViewById(R.id.ib_content_open);
 
+        if (mMessage.isIncoming()) {
+            videoTitle.setTextColor(mContext.getResources().getColorStateList(android.R.color.black));
+            videoDescription.setTextColor(mContext.getResources().getColorStateList(android.R.color.black));
+            playButton.setImageDrawable(mContext.getResources().getDrawable(R.drawable.ic_dark_video));
+        } else {
+            videoTitle.setTextColor(mContext.getResources().getColorStateList(android.R.color.white));
+            videoDescription.setTextColor(mContext.getResources().getColorStateList(android.R.color.white));
+            playButton.setImageDrawable(mContext.getResources().getDrawable(R.drawable.ic_light_video));
+        }
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (contentObject.isContentAvailable()) {
+                    String url = contentObject.getContentUrl();
+                    if (url == null) {
+                        url = contentObject.getContentDataUrl();
+                    }
+                    if (url != null) {
+                        try {
+                            Intent intent = new Intent(Intent.ACTION_VIEW);
+                            intent.setDataAndType(Uri.parse(url), "video/*");
+                            XoActivity activity = (XoActivity) mContext;
+                            activity.startExternalActivity(intent);
+                        } catch (ActivityNotFoundException exception) {
+                            Toast.makeText(mContext, R.string.error_no_videoplayer, Toast.LENGTH_LONG).show();
+                            LOG.error("Exception while starting external activity ", exception);
+                        }
+                    }
+                }
+            }
+        });
     }
 
 
