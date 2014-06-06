752a104421de50728ac498478904977fb8a403f0
Julian Krumow <julian.krumow@artcom.de>
2014-06-06 12:41:00 +0200

    - removed superfluous classes

--- src/main/java/com/hoccer/xo/android/view/chat/attachments/IClickableImageViewListener.java
+++
@@ -1,9 +0,0 @@
-package com.hoccer.xo.android.view.chat.attachments;
-
-
-public interface IClickableImageViewListener {
-
-    public void onImageViewClick(ClickableImageView view);
-
-    public void onImageViewLongClick(ClickableImageView view);
-}
