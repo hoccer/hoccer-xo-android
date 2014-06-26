79479e2f807c41ef16a436ffab910756674db9ee
Julian Krumow <julian.krumow@artcom.de>
2014-06-26 17:27:05 +0200

    - creating fragments for ContactsPager before first scrolling

--- src/main/java/com/hoccer/xo/android/adapter/ContactsPageAdapter.java
+++ src/main/java/com/hoccer/xo/android/adapter/ContactsPageAdapter.java
@@ -16,25 +16,20 @@ public class ContactsPageAdapter extends FragmentPagerAdapter {
     public ContactsPageAdapter(FragmentManager fm, int count) {
         super(fm);
         mCount = count;
+
+        mContactsFragment = new ContactsFragment();
+        mFriendRequestFragment = new FriendRequestFragment();
+        mNearbyChatFragment = new NearbyChatFragment();
     }
 
     @Override
     public Fragment getItem(int position) {
         switch (position) {
             case 0:
-                if (mContactsFragment == null) {
-                    mContactsFragment = new ContactsFragment();
-                }
                 return mContactsFragment;
             case 1:
-                if (mFriendRequestFragment == null) {
-                    mFriendRequestFragment = new FriendRequestFragment();
-                }
                 return mFriendRequestFragment;
             case 2:
-                if (mNearbyChatFragment == null) {
-                    mNearbyChatFragment = new NearbyChatFragment();
-                }
                 return mNearbyChatFragment;
         }
         return null;
