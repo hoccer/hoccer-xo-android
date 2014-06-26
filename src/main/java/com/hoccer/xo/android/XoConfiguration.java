472cc863b1d738724477b88a9870516bc0bc766a
jacob <jacob.zschunke@artcom.de>
2014-06-26 09:26:58 +0200

    removed filecache routing for test version

--- src/main/java/com/hoccer/xo/android/XoConfiguration.java
+++ src/main/java/com/hoccer/xo/android/XoConfiguration.java
@@ -26,7 +26,7 @@ public class XoConfiguration {
     public static final String INTERNAL_THUMBNAILS = "thumbnails";
 
     /* Enable or disable development settings in preferences */
-    public static final boolean DEVELOPMENT_MODE_ENABLED = true;
+    public static final boolean DEVELOPMENT_MODE_ENABLED = false;
 
     /**
      * Background executor thread count
