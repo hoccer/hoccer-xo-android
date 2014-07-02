e340d22576402ee4e7a01cd75af69eec5e442e1e
Julian Krumow <julian.krumow@artcom.de>
2014-07-02 13:22:21 +0200

    - added NearbyHistoryMessagingFragment - cleaned up NearbyChatFragment

--- src/main/java/com/hoccer/xo/android/fragment/NearbyHistoryMessagingFragment.java
+++ src/main/java/com/hoccer/xo/android/fragment/NearbyHistoryMessagingFragment.java
@@ -1,12 +1,10 @@
 package com.hoccer.xo.android.fragment;
 
+
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import com.hoccer.talk.client.IXoContactListener;
 import com.hoccer.talk.client.model.TalkClientContact;
@@ -19,34 +17,19 @@ import org.apache.log4j.Logger;
 import java.sql.SQLException;
 import java.util.List;
 
+public class NearbyHistoryMessagingFragment extends XoListFragment implements IXoContactListener {
 
-public class NearbyChatFragment extends XoListFragment implements IXoContactListener {
     private static final Logger LOG = Logger.getLogger(NearbyChatFragment.class);
+
     private NearbyChatAdapter mNearbyAdapter;
     private OverscrollListView mList;
-    private ImageView mPlaceholderImage;
-    private TextView mPlaceholderText;
-    private CompositionFragment mCompositionFragment;
-
-    private TextView mUserCountText;
-
-    private View mNearbyInfoContainer;
-    private RelativeLayout mCompositionFragmentContainer;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_nearby_chat, container, false);
+        View view = inflater.inflate(R.layout.fragment_nearby_history_messaging, container, false);
+
         mList = (OverscrollListView) view.findViewById(android.R.id.list);
-        mPlaceholderImage = (ImageView) view.findViewById(R.id.iv_contacts_placeholder);
-        mPlaceholderImage.setImageResource(R.drawable.placeholder_nearby);
-        mPlaceholderText = (TextView) view.findViewById(R.id.tv_contacts_placeholder);
-        mPlaceholderText.setText(R.string.placeholder_nearby_text);
-        mUserCountText = (TextView) view.findViewById(R.id.tv_nearby_usercount);
-        mNearbyInfoContainer = view.findViewById(R.id.rl_nearby_info);
-        mCompositionFragmentContainer = (RelativeLayout) view.findViewById(R.id.fragment_container);
-        mCompositionFragment = new CompositionFragment();
-        FragmentTransaction transaction = getFragmentManager().beginTransaction();
-        transaction.add(R.id.fragment_container, mCompositionFragment).commit();
+
         return view;
     }
 
@@ -60,7 +43,6 @@ public class NearbyChatFragment extends XoListFragment implements IXoContactList
     @Override
     public void onResume() {
         super.onResume();
-        showPlaceholder();
         converseWithContact();
     }
 
@@ -73,32 +55,6 @@ public class NearbyChatFragment extends XoListFragment implements IXoContactList
     public void onDestroy() {
         mNearbyAdapter.onDestroy();
         super.onDestroy();
-    }
-
-    public void showPlaceholder() {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                mPlaceholderImage.setVisibility(View.VISIBLE);
-                mPlaceholderText.setVisibility(View.VISIBLE);
-                mList.setVisibility(View.GONE);
-                mNearbyInfoContainer.setVisibility(View.GONE);
-                mCompositionFragmentContainer.setVisibility(View.GONE);
-            }
-        });
-    }
-
-    private void hidePlaceholder() {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                mPlaceholderImage.setVisibility(View.GONE);
-                mPlaceholderText.setVisibility(View.GONE);
-                mList.setVisibility(View.VISIBLE);
-                mNearbyInfoContainer.setVisibility(View.VISIBLE);
-                mCompositionFragmentContainer.setVisibility(View.VISIBLE);
-            }
-        });
     }
 
     public void converseWithContact() {
@@ -116,27 +72,18 @@ public class NearbyChatFragment extends XoListFragment implements IXoContactList
             if (mActivity == null) {
                 return;
             }
+
+            // TODD: find all nearby messages
             final List<TalkClientContact> nearbyGroups = getXoDatabase().findAllNearbyGroups();
-            final List<TalkClientContact> allNearbyContacts = getXoDatabase().findAllNearbyContacts();
             if (nearbyGroups.size() > 0) {
                 mActivity.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        hidePlaceholder();
+
                         final TalkClientContact nearbyGroup = nearbyGroups.get(0);
                         mNearbyAdapter.setConverseContact(nearbyGroup);
                         mNearbyAdapter.requestReload();
                         mNearbyAdapter.notifyDataSetChanged();
-                        mCompositionFragment.converseWithContact(nearbyGroup);
-
-                        mUserCountText.setText(mActivity.getResources().getString(
-                                R.string.nearby_info_usercount, allNearbyContacts.size() - 1));
-                        mUserCountText.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                mActivity.showContactProfile(nearbyGroup);
-                            }
-                        });
                     }
                 });
             }
@@ -174,6 +121,5 @@ public class NearbyChatFragment extends XoListFragment implements IXoContactList
     public void onGroupMembershipChanged(TalkClientContact contact) {
         updateViews();
     }
-
 
 }
