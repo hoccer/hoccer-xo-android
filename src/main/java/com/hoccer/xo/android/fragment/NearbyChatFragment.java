cf5c53ba18d236e8b5f3138ab130eb6d5599a956
Igor Zubov <igor.zubov@artcom.de>
2014-07-02 14:26:54 +0200

    removed usless comments

--- src/main/java/com/hoccer/xo/android/fragment/NearbyChatFragment.java
+++ src/main/java/com/hoccer/xo/android/fragment/NearbyChatFragment.java
@@ -1,5 +1,6 @@
 package com.hoccer.xo.android.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
